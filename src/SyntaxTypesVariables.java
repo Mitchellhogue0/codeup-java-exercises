9ec8319e8751a9cfb7b0c137b32c68b64f23f7d5
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-06-21 17:09:43 -0500

    commits types and variables exercise

---
+++ src/SyntaxTypesVariables.java
@@ -0,0 +1,56 @@
+public class SyntaxTypesVariables {
+
+    public static void main(String[] args) {
+
+//        byte myFirstNumber = 127;
+////        System.out.println(myFirstNumber + 1);
+//        var myString= "Hello! :)";
+//        System.out.println("Hello, my name is Mitchell :)");
+//        System.out.println(myString);
+
+        int myFavoriteNumber = 27;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "3.14159";
+        System.out.println(myString);
+
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+
+        System.out.println("--------");
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+
+//        int x = 4;
+////        x = x + 5;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+////        y = y * x;
+//        x *= y;
+//        System.out.println(x);
+
+//        int x = 10;
+//        int y = 2;
+////        x = x / y;
+////        y = y - x;
+//        x /= y;
+//        y -= x;
+
+
+
+
+    }
+
+}
