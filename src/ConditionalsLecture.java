948c76c70c9ea7fc4a300df82d8a476e6faaa11f
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-06-23 12:27:06 -0500

    commits loops exercise

---
+++ src/ConditionalsLecture.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class ConditionalsLecture {
+
+    public static void main(String[] args) {
+
+
+        //        MINI EXERCISE
+        // TODO: create three unique usernames (user1, user2, user3).
+        //  create a fourth user and set it equal to user input. (ask
+        //  the user to enter a username). if the username has already
+        //  been used by user1, user2, or user3, print "sorry this
+        //  username already exists.
+
+        String user1 = "WahtDidney";
+        String user2 = "YaasterModa";
+        String user3 = "Adriek302";
+
+        System.out.println("Please enter your username: ");
+        Scanner sc = new Scanner(System.in);
+        String user4 = sc.next();
+
+//        if (user4.equalsIgnoreCase(user1)){
+//            System.out.println("That username is already taken");
+//        }
+//
+//        if (user4.equalsIgnoreCase(user2)){
+//            System.out.println("That username is already taken");
+//        }
+//
+//        if (user4.equalsIgnoreCase(user3)){
+//            System.out.println("That username is already taken");
+//        }
+
+//        switch (user4) {
+//            case "WahtDidney":
+//                System.out.println("That username is already taken");
+//                break;
+//            case "YaasterModa":
+//                System.out.println("That username is already taken");
+//                break;
+//            case "Adriek302":
+//                System.out.println("That username is already taken");
+//                break;
+//            default:
+//                System.out.println("username accepted!");
+//                break;
+//        }
+
+        //Caseys alternate trying to use variables in switch
+//        switch (true) {
+//            case user4.equalsIgnoreCase(user1):
+//                System.out.println("That username is already taken");
+//                break;
+//            case user4.equalsIgnoreCase(user2):
+//                System.out.println("That username is already taken");
+//                break;
+//            case user4.equalsIgnoreCase(user3):
+//                System.out.println("That username is already taken");
+//                break;
+//            default:
+//                System.out.println("username accepted!");
+//                break;
+//        }
+
+        //TODO: using previous mini exercise, allow the user to input a new username several times until a unique username is entered.
+
+
+        for(String i = user4; i.equalsIgnoreCase(user1) || i.equalsIgnoreCase(user2) || i.equalsIgnoreCase(user3); ) {
+            System.out.println("That username is already taken");
+        }
+        System.out.println("That user name is accepted!");
+
+
+    }
+}
