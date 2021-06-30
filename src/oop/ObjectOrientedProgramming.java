949e5d94c0655fc5af4ddb14eaad2283bec23c95
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-06-30 11:11:54 -0500

    commits oop lecture

---
+++ src/oop/ObjectOrientedProgramming.java
@@ -0,0 +1,14 @@
+package oop;
+
+public class ObjectOrientedProgramming {
+
+    public static void main(String[] args) {
+        // tell me about a person
+//        Person person = new Person();
+
+//        person.setName("Mitchell");
+//
+//        System.out.println(person.getName());
+
+    }
+}
