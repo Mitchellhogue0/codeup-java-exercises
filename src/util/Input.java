650defbce01b171be013e90e79a21b2f1e3a70f0
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-01 16:58:55 -0500

    commits input exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,10 +1,61 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
 
-    private String scanner;
+    private Scanner scanner;
+    private int number;
+    private double doubleNum;
 
-    public Input(String scanner) {
-        this.scanner = scanner;
+
+    public Input() {
+        this.scanner = new Scanner(System.in);
     }
+
+    public String getString(){
+        System.out.println("Please enter a string: ");
+        return scanner.nextLine();
+    }
+
+    public boolean yesNo() {
+        System.out.println("Yes or no?");
+        String answer = scanner.next().toLowerCase();
+        if (answer.equals("y") || answer.equals("yes")){
+            return true;
+        }
+        return false;
+    }
+
+    public int getInt(int min, int max){
+        System.out.printf("Please pick a number between %s and %s\n", min, max);
+        number = scanner.nextInt();
+        if (number < min){
+            getInt(min,max);
+        } else if (number > max){
+            getInt(min,max);
+        }
+        return this.number;
+    }
+
+    public double getDouble(double min, double max){
+        System.out.printf("Please pick a number between %s and %s\n", min, max);
+        number = scanner.nextInt();
+        if (number < min){
+            getDouble(min,max);
+        } else if (number > max){
+            getDouble(min,max);
+        }
+        return this.number;
+    }
+
+
+
+
+
+
+
+
+
+
 }
