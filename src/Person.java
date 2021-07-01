4d08dab413a268455beac70f5787e412f8a26ca9
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-01 09:52:06 -0500

    commits oop exercises

---
+++ src/Person.java
@@ -0,0 +1,52 @@
+public class Person {
+
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person() {
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayHello(String name) {
+        System.out.printf("Hello, %s!", name);
+    }
+
+
+
+    public static void main(String[] args) {
+        Person person = new Person();
+//        person.setName("Mitchell");
+//        person.sayHello(person.getName());
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+
+
+    }
+}
---
+++ src/util/Input.java
@@ -0,0 +1,10 @@
+package util;
+
+public class Input {
+
+    private String scanner;
+
+    public Input(String scanner) {
+        this.scanner = scanner;
+    }
+}
