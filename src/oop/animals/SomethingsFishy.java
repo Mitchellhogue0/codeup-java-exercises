8d43b389ebdafd515651e8ea4c04bf944c26ddc6
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-01 15:22:50 -0500

    commits polymorphism exercise setup

---
+++ src/oop/animals/SomethingsFishy.java
@@ -0,0 +1,22 @@
+package oop.animals;
+
+public class SomethingsFishy {
+
+    public static void main(String[] args) {
+
+//        MarineAnimal blueWhale = new AquaMamm("Blue Whale");
+//
+//        System.out.println(blueWhale.getName());
+
+        MarineAnimal marineAnimal;
+        boolean printedAllMarineLife = false;
+
+        while (!printedAllMarineLife){
+            //check animal type/name
+            //pump to database
+            //next loop
+            //check to see if the list of animals is now empty
+
+        }
+    }
+}
