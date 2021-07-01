9f36edb0cd08b2611483767d40c1f039218cabda
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-01 16:16:11 -0500

    commits polymorphism exercise

---
+++ src/shapes/Square.java
@@ -0,0 +1,19 @@
+package shapes;
+
+public class Square extends Rectangle{
+
+   public Square(int side){
+       this.length = side;
+       this.width = side;
+   }
+
+    @Override
+    public int getArea() {
+        return super.getArea();
+    }
+
+    @Override
+    public int getPerimeter() {
+        return super.getPerimeter();
+    }
+}
