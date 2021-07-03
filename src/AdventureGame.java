44e53a175689b75432133c273f3a9ebf16b57249
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-03 13:25:18 -0500

    commits adventure game updates

--- src/AdventureGame.java
+++ src/AdventureGame.java
@@ -37,14 +37,8 @@ public class AdventureGame {
 
     public static void beginningRoomLit(){
        String input;
-       boolean shouldDescribeRoom = true;
 
-       if (shouldDescribeRoom) {
-           System.out.println("You light a match to see your surroundings. You're in a dark, stone-walled basement with" +
-                   " no light in any direction, you see a door in front of you, and to your left there is a picture of " +
-                   "a man in a striped green shirt smiling with his dog");
-       }
-        commands(", open door");
+
         input = UI.nextLine();
         if (input.equals("open door")) {
             System.out.println("You open the door and begin to ascend the wooden stairs in front of you");
@@ -52,17 +46,14 @@ public class AdventureGame {
         } else if (input.equals("look around") || input.equals("la")){
             System.out.println("You can't see much, but you think you might see a matchbox lying on the ground");
             commands(", pick up matchbox");
-            shouldDescribeRoom = false;
             beginningRoomLit();
         } else if (input.equals("check inventory") || input.equals("ci")){
             System.out.printf("potions: %s\n", potion);
-            shouldDescribeRoom = false;
             beginningRoomLit();
         } else if (input.equals("heal") || input.equals("h")){
             potion = potion - 1;
             HP = maxHP;
             System.out.print("You are now at max HP, HP=" + HP);
-            shouldDescribeRoom = false;
             beginningRoomLit();
         }
     }
@@ -71,45 +62,47 @@ public class AdventureGame {
 
     public static void beginningRoom(){
         String input;
-        boolean shouldDescribeRoom = true;
 
-        if (shouldDescribeRoom = true) {
-            System.out.println("You wake up on the floor, completely forgetting how you got here or where you are.");
-            wait(3);
-            System.out.println("The room is very dark, you can barely see a thing. It is very musty and hard to breathe, you" +
-                    " fear you might not be alone");
-        }
-        wait(2);
-        commands();
+
         input = UI.nextLine();
         if(input.equals("look around")){
             System.out.println("You can't see much, but you think you might see a matchbox lying on the ground");
             commands(", pick up matchbox");
-            shouldDescribeRoom = false;
             beginningRoom();
         } else if (input.equals("heal")){
             potion = potion - 1;
             HP = maxHP;
             System.out.printf("You are now at max HP, HP=%s\n", HP);
-            shouldDescribeRoom = false;
             beginningRoom();
         } else if (input.equals("pick up matchbox")){
-            beginningRoomLit();
+            router("beginningRoomLit");
         } else if (input.equals("check inventory")){
             System.out.printf("potions: %s\n", potion);
-            shouldDescribeRoom = false;
             beginningRoom();
         } else{
             System.out.println("Please choose from the commands");
             commands();
             wait(2);
-            shouldDescribeRoom = false;
             beginningRoom();
         }
     }
 
-    public static void router(){
-
+    public static void router(String room){
+        if (room.equals("beginningRoom")) {
+            System.out.println("You wake up on the floor, completely forgetting how you got here or where you are.");
+            wait(3);
+            System.out.println("The room is very dark, you can barely see a thing. It is very musty and hard to breathe, you" +
+                    " fear you might not be alone");
+            wait(2);
+            commands();
+            beginningRoom();
+        } else if (room.equals("beginningRoomLit")){
+            System.out.println("You light a match to see your surroundings. You're in a dark, stone-walled basement with" +
+                    " no light in any direction, you see a door in front of you, and to your left there is a picture of " +
+                    "a man in a striped green shirt smiling with his dog");
+            commands(", open door");
+            beginningRoomLit();
+        }
     }
 
     public static void commands(){
@@ -148,6 +141,6 @@ public class AdventureGame {
         lvl = 1;
         atk = 1;
         def = 1;
-        beginningRoom();
+        router("beginningRoom");
     }
 }
