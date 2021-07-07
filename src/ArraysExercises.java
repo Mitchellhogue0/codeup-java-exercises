1c0c126875fc8930156b1f5ccf98bca7205d517f
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-07 11:59:55 -0500

    commits arrays exercise part one

---
+++ src/ArraysExercises.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+
+    public static void main(String[] args) {
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(Arrays.toString(numbers));
+
+        String[] Persons = {"Karen", "Kyle", "Chad"};
+
+        for (String person : Persons) {
+//            System.out.println(person);
+           String Person = "Tabitha";
+            System.out.println(Arrays.toString(addPerson(Person, Persons)));
+        }
+    }
+
+    public static String[] addPerson(String person, String[] persons){
+        persons = Arrays.copyOf(persons, persons.length + 1);
+        persons[persons.length -1] = person;
+        return persons;
+    }
+}
