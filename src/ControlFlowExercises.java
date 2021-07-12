c80684f15403781160569fb27ee43660864abcb8
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-12 14:24:36 -0500

    commits Control Flow two

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,20 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15){
-            System.out.println(i);
-            i++;
-        }
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+//        int i = 100;
+//        do {
+//            i -= 5;
+//        } while (i > 0);
+//        System.out.println(i);
+        float i = 2;
+        do {
+            i = i * i;
+        System.out.println(i);
+        } while (i < 1000000);
     }
 }
