51b18335ea271997cb5923aeb9df600360c361fb
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-12 14:30:19 -0500

    commits Control Flow 3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -11,10 +11,18 @@ public class ControlFlowExercises {
 //            i -= 5;
 //        } while (i > 0);
 //        System.out.println(i);
-        float i = 2;
-        do {
-            i = i * i;
-        System.out.println(i);
-        } while (i < 1000000);
+//        float i = 2;
+//        do {
+//            i = i * i;
+//        System.out.println(i);
+//        } while (i < 1000000);
+
+//        for (int i =100; i > 0; i-= 5) {
+//            System.out.println(i);
+//        }
+
+        for (double i = 2; i < 1000000; i *= i) {
+            System.out.println(i);
+        }
     }
 }
