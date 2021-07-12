fa7b0901adb98eb35c3d628df0d33a67dc9e0cee
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-12 14:42:39 -0500

    commits Control flow 4

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -21,8 +21,20 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
-        for (double i = 2; i < 1000000; i *= i) {
-            System.out.println(i);
+//        for (double i = 2; i < 1000000; i *= i) {
+//            System.out.println(i);
+//        }
+
+        for (int i = 0; i < 100; i++) {
+            if (i % 15 == 0){
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0){
+                System.out.println("Buzz");
+            }else if (i % 3 == 0){
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
