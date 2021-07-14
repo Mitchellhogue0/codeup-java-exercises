2698da4266a243ae393fcb8d6fb0c899df1d3a08
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-14 10:21:25 -0500

    commits Exception lecture set up

---
+++ src/ExeceptionAndErrorHandling.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ExeceptionAndErrorHandling {
+
+    public static void main(String[] args) {
+//        int myNum = Integer.parseInt("eighteen");
+
+
+//        ArrayList<Integer> myArrList = new ArrayList<Integer>();
+//        myArrList.get(2);
+
+
+    }
+}
--- src/warmups/StringToArray.java
+++ src/warmups/StringToArray.java
@@ -2,13 +2,16 @@ package warmups;
 
 import java.util.Arrays;
 
+import java.util.ArrayList;
+
 public class StringToArray {
 
     public static void main(String[] args) {
         // warmup: write a method that accepts a string and returns a String array that splits at the spaces - you can
         // write this within any main method
 
-        System.out.println(Arrays.toString(arrayConversion("Let's make sure that that this method works")));
+//        System.out.println(Arrays.toString(arrayConversion("Let's make sure that that this method works")));
+
     }
 
     public static String[] arrayConversion ( String string){
