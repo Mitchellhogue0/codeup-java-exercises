60490dfcb6dde79ffeb7ff8bc8f23d0fee5612f9
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-15 15:38:32 -0500

    commits IO lecture refactor

---
+++ src/fileIO/FileDirectoryUtil.java
@@ -0,0 +1,52 @@
+package fileIO;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileDirectoryUtil {
+
+    public static Path getPath(String fileName) {
+        return Paths.get(fileName);
+    }
+
+
+    public static Path getPath(String parentDirectory, String fileName) {
+        return Paths.get(parentDirectory, fileName);
+    }
+
+
+    public static Path getPath(String parentDirectory, String childDirectory, String fileName) {
+        return Paths.get(parentDirectory, childDirectory, fileName);
+    }
+
+    public static boolean doesPathExist(Path path) {
+        return Files.exists(path);
+    }
+
+    public static void tryCreateDirectory(Path pathToCreate) {
+        if (!doesPathExist(pathToCreate)) {
+            try {
+                Files.createDirectory(pathToCreate);
+
+            } catch (IOException e) {
+                System.out.println("Could not create Directory");
+                System.out.println(pathToCreate.toAbsolutePath());
+            }
+        } else {
+            System.out.println("The path at: " + pathToCreate.toAbsolutePath() + " already exists");
+        }
+    }
+
+    public static void tryCreateFile (Path path) {
+        if (!doesPathExist(path)){
+            try {
+                Files.createFile(path);
+            } catch (IOException e){
+                System.out.println("Could not create file at: " + path.toAbsolutePath() + " already exists");
+            }
+        }
+    }
+
+}
