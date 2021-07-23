5017e3231f303e333ccee9e6e540231a70114018
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-23 16:36:02 -0500

    commits adventureGame refactor

--- src/adventureGame/AdventureGame.java
+++ src/adventureGame/AdventureGame.java
@@ -1,3 +1,5 @@
+package adventureGame;
+
 import java.util.Random;
 import java.util.Scanner;
 
---
+++ src/adventureGame/Hostile.java
@@ -0,0 +1,14 @@
+package adventureGame;
+
+public class Hostile {
+
+    public static String name;
+    public static String type;
+    public static int lvl;
+
+
+    public Hostile() {
+    }
+
+
+}
